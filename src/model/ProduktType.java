158ed580bcfb92b70474906a53f16a79343e1910
ArosanSB <asb190401@gmail.com>
2021-10-13 13:26:23 +0200

    SalgsSituation

--- src/model/ProduktType.java
+++ src/model/ProduktType.java
@@ -1,4 +1,37 @@
 package model;
 
+import java.util.ArrayList;
+
 public class ProduktType {
+    private String navn;
+    private ArrayList<Produkt> produkter = new ArrayList<Produkt>();
+
+    public ProduktType(String navn) {
+        this.navn = navn;
+    }
+
+    public String getNavn() {
+        return navn;
+    }
+
+    public void setNavn(String navn) {
+        this.navn = navn;
+    }
+
+    public ArrayList<Produkt> getProdukter() {
+        return produkter;
+    }
+
+    public void addProdukter(ArrayList<Produkt> produkt) {
+        if (!produkter.contains(produkt)) {
+            produkter.add(produkt);
+            produkter.setProduktType(this);
+        }
+
+}
+
+    @Override
+    public String toString() {
+        return navn;
+    }
 }
