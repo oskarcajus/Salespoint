d9942c627041c0ad0c766ea3ca6865938a21dd11
OskarCajus <cajus2@live.dk>
2021-10-14 09:02:44 +0200

    Produkt changes

---
+++ src/gui/MainApp.java
@@ -0,0 +1,77 @@
+package gui;
+
+
+    import controller.Controller;
+    import javafx.application.Application;
+    import javafx.scene.Scene;
+    import javafx.scene.control.Tab;
+    import javafx.scene.control.TabPane;
+    import javafx.scene.control.TabPane.TabClosingPolicy;
+    import javafx.scene.layout.BorderPane;
+    import javafx.stage.Stage;
+
+    public class MainApp extends Application {
+
+        // Controller access
+        private Controller controller;
+
+//------------------------------------------------------------------------------------------------
+
+        @Override
+        public void init() {
+//            controller = Controller.getController();
+        }
+
+        //------------------------------------------------------------------------------------------------
+        @Override
+        public void start(Stage stage) {
+
+            stage.setTitle("Salespoint");
+            BorderPane pane = new BorderPane();
+            this.initContent(pane);
+
+            Scene scene = new Scene(pane);
+            stage.setScene(scene);
+            stage.setHeight(500);
+            stage.setWidth(800);
+            stage.show();
+        }
+
+//------tabPane------------------------------------------------------------------------------------------
+
+        private void initContent(BorderPane pane) {
+            TabPane tabPane = new TabPane();
+//            this.initTabPane(tabPane);
+            pane.setCenter(tabPane);
+        }
+
+//-------init tabPane-----------------------------------------------------------------------------------------
+
+//        private void initTabPane(TabPane tabPane) {
+//
+//            tabPane.setTabClosingPolicy(TabClosingPolicy.UNAVAILABLE);
+//
+//            // forskellige tab
+//            Tab tabOrdre = new Tab("Ordre Admin");
+//            Tab tabProdukter = new Tab("Produkter");
+//            Tab tabPrisLister = new Tab("PrisLister");
+//
+//            // Ordre admin pane til tab
+//            OrdreAdminPane ordrePane = new OrdreAdminPane();
+//            tabOrdre.setContent(ordrePane);
+//
+//            // ProduktPane til tab
+//            ProduktPane produktPane = new ProduktPane();
+//            tabProdukter.setContent(produktPane);
+//
+//            // PrisListePane til tab
+//            PrisListePane prisListePane = new PrisListePane();
+//            tabPrisLister.setContent(prisListePane);
+//
+//            // Add Ordre, produkter, pristLister til TabPane
+//            tabPane.getTabs().add(tabOrdre);
+//            tabPane.getTabs().add(tabProdukter);
+//            tabPane.getTabs().add(tabPrisLister);
+//        }
+    }
+
