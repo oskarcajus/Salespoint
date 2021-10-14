da75a4b470fa7458499e05cb1f72a399bd89524a
OskarCajus <cajus2@live.dk>
2021-10-14 10:15:25 +0200

    Startskærm med billeder

--- src/gui/AdminWindow.java
+++ src/gui/AdminWindow.java
@@ -3,24 +3,19 @@ package gui;
 import controller.Controller;
 import javafx.application.Application;
 import javafx.scene.Scene;
+import javafx.scene.control.Tab;
 import javafx.scene.control.TabPane;
 import javafx.scene.layout.BorderPane;
 import javafx.stage.Stage;
 
-    public class AdminWindow extends Application {
+    public class AdminWindow extends Stage {
 
         // Controller access
         private Controller controller;
 
 //------------------------------------------------------------------------------------------------
 
-        @Override
-        public void init() {
-//            controller = Controller.getController();
-        }
-
         //------------------------------------------------------------------------------------------------
-        @Override
         public void start(Stage stage) {
 
             stage.setTitle("Admin");
@@ -38,7 +33,34 @@ import javafx.stage.Stage;
 
         private void initContent(BorderPane pane) {
             TabPane tabPane = new TabPane();
-//            this.initTabPane(tabPane);
+            this.initTabPane(tabPane);
             pane.setCenter(tabPane);
+        }
+
+        private void initTabPane(TabPane tabPane) {
+
+            tabPane.setTabClosingPolicy(TabPane.TabClosingPolicy.UNAVAILABLE);
+
+            // forskellige tab
+            Tab tabOrdre = new Tab("Ordre Admin");
+            Tab tabProdukter = new Tab("Produkter");
+            Tab tabPrisLister = new Tab("PrisLister");
+
+            // Ordre admin pane til tab
+//            OrdreAdminPane ordrePane = new OrdreAdminPane();
+//            tabOrdre.setContent(ordrePane);
+//
+//            // ProduktPane til tab
+//            ProduktPane produktPane = new ProduktPane();
+//            tabProdukter.setContent(produktPane);
+//
+//            // PrisListePane til tab
+//            PrisListePane prisListePane = new PrisListePane();
+//            tabPrisLister.setContent(prisListePane);
+
+            // Add Ordre, produkter, pristLister til TabPane
+            tabPane.getTabs().add(tabOrdre);
+            tabPane.getTabs().add(tabProdukter);
+            tabPane.getTabs().add(tabPrisLister);
         }
 }
--- src/gui/MainApp.java
+++ src/gui/MainApp.java
@@ -8,6 +8,9 @@ import javafx.geometry.Insets;
 import javafx.geometry.Pos;
 import javafx.scene.Scene;
 import javafx.scene.control.Button;
+import javafx.scene.control.ContentDisplay;
+import javafx.scene.image.Image;
+import javafx.scene.image.ImageView;
 import javafx.scene.layout.BorderPane;
 import javafx.scene.layout.GridPane;
 import javafx.scene.layout.Priority;
@@ -18,53 +21,80 @@ import javafx.scene.layout.HBox;
 
 public class MainApp extends Application {
 
-        // Controller access
-        private Controller controller;
+    // Controller access
+    private Controller controller;
 
 //------------------------------------------------------------------------------------------------
 
-        @Override
-        public void init() {
+    @Override
+    public void init() {
 //            controller = Controller.getController();
-        }
-
-        //------------------------------------------------------------------------------------------------
-        @Override
-        public void start(Stage stage) {
-
-            stage.setTitle("Salespoint");
-            GridPane pane = new GridPane();
-            this.initContent(pane);
-
-            Scene scene = new Scene(pane);
-            stage.setScene(scene);
-            stage.setHeight(200);
-            stage.setWidth(300);
-            stage.show();
-        }
-
-        // Buttons
-        private Button btnAdmin = new Button("Admin");
-        private Button btnSalg = new Button("Salg System");
-
-//------tabPane------------------------------------------------------------------------------------------
-
-        private void initContent(GridPane pane) {
-            pane.setGridLinesVisible(false);
-            pane.setAlignment(Pos.CENTER);
-
-            pane.add(btnAdmin, 5, 4);
-            pane.setMargin(btnAdmin, new Insets(10, 10, 10, 10));
-            pane.setHalignment(btnAdmin, HPos.CENTER);
-            btnAdmin.setMinSize(100, 100);
-//            btnAdmin.setOnAction(event -> rollAction());
-
-            pane.add(btnSalg, 6, 4);
-            pane.setMargin(btnSalg, new Insets(10, 10, 10, 10));
-            pane.setHalignment(btnSalg, HPos.CENTER);
-            btnSalg.setMinSize(100, 100);
-//            btnAdmin.setOnAction(event -> rollAction());
-
-        }
     }
+
+    //------------------------------------------------------------------------------------------------
+    @Override
+    public void start(Stage stage) {
+
+        stage.setTitle("Salespoint");
+        GridPane pane = new GridPane();
+        this.initContent(pane);
+
+        Scene scene = new Scene(pane);
+        stage.setScene(scene);
+        stage.setHeight(400);
+        stage.setWidth(600);
+        stage.show();
+    }
+
+    // Buttons
+    private Button btnAdmin;
+    private Button btnSalg;
+
+    private void initContent(GridPane pane) {
+        pane.setGridLinesVisible(false);
+        pane.setAlignment(Pos.CENTER);
+
+        //Buttons ----------------------
+
+        //Admin button
+        String  imageAdmin= "https://cdn4.iconfinder.com/data/icons/forgen-phone-settings/48/setting-512.png";
+        Image imAdmin = new Image(imageAdmin, 100, 100, false, false);
+        ImageView imageViewAdmin = new ImageView(imAdmin);
+
+        btnAdmin = new Button("Admin", imageViewAdmin);
+
+        pane.add(btnAdmin, 5, 4);
+        btnAdmin.setContentDisplay(ContentDisplay.TOP);
+        pane.setMargin(btnAdmin, new Insets(10, 10, 10, 10));
+        pane.setHalignment(btnAdmin, HPos.CENTER);
+        btnAdmin.setMaxSize(100, 100);
+
+        btnAdmin.setOnAction(event -> openAdminWindow());
+
+        //Salgs button
+        String  imageSalg= "http://simpleicon.com/wp-content/uploads/shop-4.png";
+        Image imSalg = new Image(imageSalg, 100, 100, false, false);
+        ImageView imageViewSalg = new ImageView(imSalg);
+
+        btnSalg = new Button("Salg", imageViewSalg);
+
+        pane.add(btnSalg, 6, 4);
+        btnSalg.setContentDisplay(ContentDisplay.TOP);
+        pane.setMargin(btnSalg, new Insets(10, 10, 10, 10));
+        pane.setHalignment(btnSalg, HPos.CENTER);
+        btnSalg.setMaxSize(100, 100);
+
+        btnSalg.setOnAction(event -> openSalgWindow());
+    }
+
+    private void openAdminWindow() {
+        System.out.println("Test Admin");
+        AdminWindow aw = new AdminWindow();
+        aw.showAndWait();
+    }
+
+    private void openSalgWindow() {
+        System.out.println("Test Salg");
+    }
+}
 
